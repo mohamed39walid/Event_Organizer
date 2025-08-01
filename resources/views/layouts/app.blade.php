d298a2604b0d7ddf129ce8bb9a9ab5cae186fa84
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-08-01 22:20:23 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;600&family=Inter:wght@400;600&family=Manrope:wght@400;600&family=Poppins:wght@400;600&family=Space+Grotesk:wght@400;700&family=Urbanist:wght@400;600&display=swap"
+        rel="stylesheet">
+
 </head>
 
 <body class="bg-bg dark:bg-dark-bg text-gray-900 dark:text-white font-sans leading-relaxed">
