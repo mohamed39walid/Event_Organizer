02c99dd7340f1753da932731c5de6a377d4703f0
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-08-01 20:02:54 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::fallback(function () {
+    return view('pages.not-found');
+});
+    
+Route::get('/test-success', function () {
+    return redirect('/')->with('success', 'Test success message!');
+});
+
+Route::get('/test-error', function () {
+    return redirect('/')->with('error', 'Test error message!');
 });
