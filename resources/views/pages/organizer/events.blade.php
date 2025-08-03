570cb6f468351253ebd245e479434e09ebf5945f
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-08-04 00:01:17 +0300

    Create events.blade.php

---
+++ resources/views/pages/organizer/events.blade.php
@@ -0,0 +1,25 @@
+
+{{-- resources/views/test OrganizerEvents.blade.php --}}
+{{---------------------- Test Page------------------------------- --}}
+
+{{-- UnCommecnt when the FrontEnd Part Ends --}}
+
+
+{{-- @if($events->isEmpty())
+    <p>No events found</p>
+@else
+    <ul>
+        @foreach($events as $event)
+            <li>{{ $event->title }}</li>
+        @endforeach
+    </ul>
+@endif --}}
+
+
+
+{{-- Should require 3 buttons on each event
+1- Edit
+2- Delete
+3- View Proposlas
+--}}
+<h1>Test Page</h1>
