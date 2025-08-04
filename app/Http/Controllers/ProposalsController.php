ebb166b3774a1e2f3fe4a785a9b78569b52ce36c
mohamed39walid <mohamed3636walid@gmail.com>
2025-08-04 16:54:06 +0300

    Proposal Controller Added but need frontend to test it

--- app/Http/Controllers/ProposalsController.php
+++ app/Http/Controllers/ProposalsController.php
@@ -2,33 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Proposal\AddedProposalRequest;
 use App\Models\Event;
 use App\Models\Proposal;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
+use function Pest\Laravel\json;
 
 class ProposalsController extends Controller
 {
-    // viewing all the proposals
-    // public function AllProposals()
-    // {
-    //     $proposals = Proposal::all();
-    //     return view('pages.organizer.proposals');
-    // }
+    public function GetSpeakerProposals()
+    {
+        $speaker_id = Auth::id();
+        $proposals = Proposal::where("speaker_id", $speaker_id)->get();
+        if (!$proposals) {
+            return json("msg", "there is no proposals");
+        }
+        return view("pages.speaker.proposals", compact("proposals"));
+    }
 
-    public function AcceptProposal($id) {
+    public function CreateProposal(AddedProposalRequest $request, $id)
+    {
+        $event = Event::find($id);
+        if (!$event) {
+            return redirect()->back()->with('error', 'Invalid event ID.');
+        }
+        $validated = $request->validated();
+        $speaker_id = Auth::id();
+        $exsiting_proposal = Proposal::where("event_id", $id)->where("speaker_id", $speaker_id)->get();
+        if ($exsiting_proposal) {
+            return redirect()->back()->with('info', 'You have already applied this event.');
+        }
+        Proposal::create([
+            "title" => $validated["title"],
+            "description" => $validated['description'],
+            "cv" => $validated['cv'],
+            "status" => "pending",
+            "speaker_id" => $speaker_id,
+            "event_id" => $id
+        ]);
+        return redirect()->route('speaker.my-proposals')->with('success', 'Proposal applied successfully.');
+    }
+    public function DeleteProposal($id)
+    {
+        $speaker_id = Auth::id();
+        $proposal = Proposal::where("speaker_id", $speaker_id)->where("id", $id)->first();
+        if (!$proposal) {
+            return redirect()->back()->with('error', "Proposal not found or doesn't belong to you.");
+        }
+        $proposal->delete();
+        return redirect()->route('speaker.my-proposals')->with('success', 'Ticket unbooked successfully.');
+    }
+    public function AcceptProposal($id)
+    {
         $proposal = Proposal::findorfail($id);
-        $proposal -> status = 'approved';
+        $proposal->status = 'approved';
 
         $proposal->save();
 
         return redirect()->route('proposals')->with('success', 'Speakers Propsal has been Approved ');
     }
-    public function RejectProposal($id) {
+    public function RejectProposal($id)
+    {
 
         $proposal = Proposal::findorfail($id);
 
-        $proposal -> status = 'rejected';
+        $proposal->status = 'rejected';
 
         $proposal->save();
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -33,8 +33,7 @@
                             class="hover:text-accent dark:hover:text-dark-accent transition duration-150 ease-in-out">
                             My Events
                         </a>
-                        <a href=""
-                        {{-- {{ route('speaker.proposals') }} --}}
+                        <a href="{{ route('speaker.my-proposals') }}"
                             class="hover:text-accent dark:hover:text-dark-accent transition duration-150 ease-in-out">
                             My Proposals
                         </a>
