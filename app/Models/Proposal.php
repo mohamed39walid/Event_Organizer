27eb3aa8594bca49f203ca64d7b2ec29c95065c5
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-08-04 23:56:26 +0300

    Update Proposal.php

--- app/Models/Proposal.php
+++ app/Models/Proposal.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proposal extends Model
 {
-    //
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+    public function speaker()
+    {
+        return $this->belongsTo(User::class, 'speaker_id');
+    }
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'cv',
+        'speaker_id',
+        'event_id',
+    ];
 }
