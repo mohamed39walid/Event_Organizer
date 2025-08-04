fac030f4bf8b26cad96475ec86f4f66d26c31448
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-08-04 23:56:29 +0300

    Update not-found.blade.php

--- resources/views/pages/shared/not-found.blade.php
+++ resources/views/pages/shared/not-found.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
     <div
-        class="min-h-[calc(100vh-140px)] flex flex-col justify-center items-center px-4 bg-background text-foreground dark:bg-dark-background dark:text-dark-foreground">
+        class="min-h-[calc(100vh-140px)] flex flex-col justify-center items-center px-4 bg-bg text-foreground dark:bg-dark-bg dark:text-dark-foreground">
         <div class="text-center space-y-6">
             <h1 class="text-8xl sm:text-9xl font-extrabold text-error dark:text-dark-error tracking-tight drop-shadow-lg">
                 404
