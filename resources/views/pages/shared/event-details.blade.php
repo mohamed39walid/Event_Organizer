963aa5ad074b8e1d6d4b66ccebc10be380869d13
AdamAhmed0 <adamshawky222@gmail.com>
2025-08-09 14:31:56 +0300

    Unbook Sweet Alert

--- resources/views/pages/shared/event-details.blade.php
+++ resources/views/pages/shared/event-details.blade.php
@@ -141,7 +141,7 @@ return 'text-green-600 dark:text-green-400';
                     </div>
 
                 </div>
-                @if ($proposals && auth()->user()->role == 'organizer')
+                @if ($proposals && auth()->user()->role == 'organizer' && $event->user_id == auth()->id())
                 <div class="mt-6 border-t border-border dark:border-dark-border pt-6">
                     <h3 class="text-lg font-semibold font-heading text-secondary dark:text-dark-secondary mb-4">
                         <i class="fa-solid fa-hand mr-2"></i> Proposal Requests
@@ -353,7 +353,7 @@ return 'text-green-600 dark:text-green-400';
                     <div id="map" style="height: 400px;"></div>
                     <p id="coordinates" class="mt-2"></p>
                     <a id="googleMapsLink" href="#" target="_blank"></a>
-                    <button id="openGoogleMapsBtn" class="btn btn-primary mt-2">
+                    <button id="openGoogleMapsBtn" class="cursor-pointer dark:text-dark-primary light:text-blue-500 btn btn-primary mt-2">
                         Open in Google Maps
                     </button>
             </div>
